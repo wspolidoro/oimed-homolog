26cc51b7b363e69b9257176ce3fbd0328169857e
devKledisom <devkledisom@gamil.com>
2025-06-09 11:01:05 -0300

    feat: webhook inativate correcao3

--- controllers/auto_ativacao/webhook.js
+++ controllers/auto_ativacao/webhook.js
@@ -1,5 +1,11 @@
+const tokenSandBox = '$aact_hmlg_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjEwM2NlODFiLTM0NWEtNGE4NS1iZjYxLTRkZWU4MWI4NTUwMjo6JGFhY2hfMGM0M2ZhZjEtOGQ3ZS00NDMwLWFhNzQtMjhjZTgzNTZmN2Fk';
+const tokenProd = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0NTA1NTM6OiRhYWNoXzJkYjI5MjQ5LWU2ODEtNDcwNC1hNzAyLWU4NGRhZDA4NzRiNg==';
+//const urlApi = "http://localhost:3035/api";
+const urlApi = "https://parceiro.painelw.com.br/api";
+
+
 function atualizarEstado(param, status) {
-    fetch(`https://apioimed.z4you.com.br/franqueado/cliente/status`,
+    fetch(`${urlApi}/franqueado/cliente/status`,
         {
             method: "PUT",
             headers: { 'Content-Type': 'application/json' },
@@ -13,7 +19,7 @@ function atualizarEstado(param, status) {
 
 
 function alterarParaAtivo(client) {
-    fetch(`https://apioimed.z4you.com.br/beneficiaries/create/${client}`,
+    fetch(`${urlApi}/beneficiaries/create/${client}`,
         {
             method: "POST",
             headers: { 'Content-Type': 'application/json' }
@@ -21,30 +27,56 @@ function alterarParaAtivo(client) {
         .then((x) => x.json())
         .then((res) => {
 
-            console.log("novo fetch: ", res);
+            console.log("novo fetch ativar: ", res);
             atualizarEstado(client, "ativo");
+
+            if (res.message === 'CPF já utilizado; ') {
+                alterarParaReativo(client)
+            }
+        }).catch(err => {
+            console.log("erro ao ativar: ", err)
         });
 };
 
 function alterarParaInativo(client) {
-    fetch(`https://apioimed.z4you.com.br/beneficiaries/create/${client}`,
+    fetch(`${urlApi}/beneficiaries/${client}`,
         {
-            method: "POST",
+            method: "DELETE",
             headers: { 'Content-Type': 'application/json' }
         })
         .then((x) => x.json())
         .then((res) => {
 
-            console.log("novo fetch: ", res);
+            console.log("novo fetch inativar: ", res);
             atualizarEstado(client, "inativo");
+
+        }).catch(err => {
+            console.log("erro ao inativar: ", err)
+        });
+};
+
+function alterarParaReativo(client) {
+    fetch(`${urlApi}/beneficiaries/reactivate/${client}`,
+        {
+            method: "PUT",
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then((x) => x.json())
+        .then((res) => {
+
+            console.log("novo fetch reativar: ", res);
+            atualizarEstado(client, "ativo");
+        }).catch(err => {
+            console.log("erro ao reativar: ", err)
         });
 };
 
 
+
 function webhookActivate(body, token, res) {
     console.log("kleidsom", token);
-    const tokenSandBox = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwODM0MjE6OiRhYWNoXzVmNzE0ZTM5LTI5NjUtNGY1MC04NjRlLTkyNGM5N2E1ZTVjMg==';
-    const tokenProd = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0NTA1NTM6OiRhYWNoXzJkYjI5MjQ5LWU2ODEtNDcwNC1hNzAyLWU4NGRhZDA4NzRiNg==';
+    //const tokenSandBox = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwODM0MjE6OiRhYWNoXzVmNzE0ZTM5LTI5NjUtNGY1MC04NjRlLTkyNGM5N2E1ZTVjMg==';
+    //const tokenProd = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0NTA1NTM6OiRhYWNoXzJkYjI5MjQ5LWU2ODEtNDcwNC1hNzAyLWU4NGRhZDA4NzRiNg==';
 
     if (body.event === 'PAYMENT_CONFIRMED' || body.event === 'PAYMENT_RECEIVED') {
         const urlSandBox = `https://sandbox.asaas.com/api/v3/customers/${body.payment.customer}`;
@@ -53,12 +85,12 @@ function webhookActivate(body, token, res) {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                access_token: token
+                access_token: tokenSandBox
             }
         };
 
 
-        fetch(urlProd, options)
+        fetch(urlSandBox, options)
             .then(res => res.json())
             .then(json => {
                 console.log(json.cpfCnpj);
@@ -75,9 +107,6 @@ function webhookActivate(body, token, res) {
 };
 
 function webhookInactivate(body, token, res) {
-    const tokenSandBox = '$aact_hmlg_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjEwM2NlODFiLTM0NWEtNGE4NS1iZjYxLTRkZWU4MWI4NTUwMjo6JGFhY2hfMGM0M2ZhZjEtOGQ3ZS00NDMwLWFhNzQtMjhjZTgzNTZmN2Fk';
-    const tokenProd = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0NTA1NTM6OiRhYWNoXzJkYjI5MjQ5LWU2ODEtNDcwNC1hNzAyLWU4NGRhZDA4NzRiNg==';
-
     if (body.event === 'PAYMENT_OVERDUE') {
         const urlSandBox = `https://sandbox.asaas.com/api/v3/customers/${body.payment.customer}`;
         const urlProd = `https://api.asaas.com/v3/customers/${body.payment.customer}`;
