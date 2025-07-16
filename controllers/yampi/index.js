2a6dfd4cfd668cf1106b252633b0374e792b8863
wspolidoro <wspolidoro@gmail.com>
2025-07-16 13:46:04 -0300

    fix: 1.0.8 - correcao de webhook

--- controllers/yampi/index.js
+++ controllers/yampi/index.js
@@ -15,7 +15,7 @@ module.exports = {
             },
             raw: true
         });
-        
+
         let orderId = cliente.order_id;
 
         const url = 'https://accounts.cartpanda.com/api/v3/conecta-med/orders/' + orderId;
@@ -32,30 +32,39 @@ module.exports = {
             const data = await response.json();
 
             let definirStatus = (statusCod) => {
-                if(statusCod == 1) return "Pendente";
-                if(statusCod == 3) return "Pago";
-                if(statusCod == 4) return "Cancelado";
+                if (statusCod == 1) return "Pendente";
+                if (statusCod == 3) return "Pago";
+                if (statusCod == 4) return "Cancelado";
             }
 
             let dividirdata = data.order.payment.updated_at;
 
             const obj = {
-                 nome: data.order.customer.first_name + " " + data.order.customer.last_name,
-                 valor: data.order.subtotal_price,
-                 dataPagamento: dividirdata.split("T")[0],
-                 status: definirStatus(data.order.payment.status_id),
-                 metodoPagamento: data.order.payment.payment_type
+                nome: data.order.customer.first_name + " " + data.order.customer.last_name,
+                valor: data.order.subtotal_price,
+                dataPagamento: dividirdata.split("T")[0],
+                status: definirStatus(data.order.payment.status_id),
+                metodoPagamento: data.order.payment.payment_type
             }
 
             //console.log({nome: nome, valor: valor, dataPagamento: dataPagamento, status: status, metodoPagamento: metodoPagamento})
 
-            res.json({success: true, data: obj})
+            res.json({ success: true, data: obj })
         } catch (error) {
             console.error(error);
         }
     },
     webhook: async (req, res) => {
         const data = req.body;
+
+        if (data.event === "order.paid") {
+            let cpf = data.order.customer.cpf;
+            const registerOrder = await CartPanda.create({
+                nu_documento: cpf.replace(/[\.-]/g, ""),
+                order_id: data.order.id,
+                status_payment: data.order.payment_status
+            })
+        }
 
         if (data.event === "order.created") {
             let cpf = data.order.customer.cpf;
