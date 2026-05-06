c54e4229414394a57816039605a2af4977e1d535
devkledisom <dev@ziiz.com.br>
2026-05-06 16:50:54 -0300

    debug8

--- routs/config/smtp.js
+++ routs/config/smtp.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: "smtp.gmail.com",
     port: 465,
+    secure: true,
     auth: {
         user: 'devkledisom@gmail.com',
         pass: "cbdq osve xpov vsio"//"ynpp dyky wvyq jhxo",//'mhhipphoaubheehg' //senha de app
